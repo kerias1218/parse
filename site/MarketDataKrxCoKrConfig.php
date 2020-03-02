ea1515a91916663f94893f289810e048a66d051a
asdf <asdf@asdf.com>
2020-03-02 11:43:20 +0900

    MarketDataKrxCoKrConfig add

--- site/MarketDataKrxCoKrConfig.php
+++ site/MarketDataKrxCoKrConfig.php
@@ -16,12 +16,16 @@ class MarketDataKrxCoKrConfig
 
     public function __construct($searchDate) {
         $ca =  Carbon::now();
-        $this->otpUrl = 'http://marketdata.krx.co.kr/contents/COM/GenerateOTP.jspx?name=fileDown&filetype=csv&url=MKD/13/1302/13020101/mkd13020101&market_gubun=ALL&sect_tp_cd=ALL&schdate='.$searchDate.'&pagePath=%2Fcontents%2FMKD%2F13%2F1302%2F13020101%2FMKD13020101.jsp';
+        $this->otpUrl = 'http://marketdata.krx.co.kr/contents/COM/GenerateOTP.jspx?name=fileDown&filetype=csv&'.
+            'url=MKD/13/1302/13020101/mkd13020101&market_gubun=ALL&sect_tp_cd=ALL&schdate='.$searchDate.
+            '&pagePath=%2Fcontents%2FMKD%2F13%2F1302%2F13020101%2FMKD13020101.jsp';
         $this->otpRefer = 'http://marketdata.krx.co.kr/contents/MKD/13/1302/13020101/MKD13020101.jsp';
         $this->downCsvUrl = 'http://file.krx.co.kr/download.jspx';
         $this->downCsvRefer = 'http://marketdata.krx.co.kr/contents/MKD/13/1302/13020101/MKD13020101.jsp';
-        $this->dir = __DIR__.'/../../../../data2/';
-        $this->saveFileName = $ca->format("Y-m-d");
+
+        $this->dir = __DIR__.'/../../../../base_data/';
+        //$this->saveFileName = $ca->format("Y-m-d");
+        $this->saveFileName = $searchDate;
         $this->ext = '.csv';
     }
 
